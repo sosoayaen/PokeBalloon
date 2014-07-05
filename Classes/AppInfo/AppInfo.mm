e6c70e3acc7df059a1d4c39f9f7b7c1a62ad333d
sosoayaen <sosoayaen@gmail.com>
2014-07-05 15:48:05 +0800

    增加iOS版本从程序配置中读取程序版本号等数据

---
+++ Classes/AppInfo/AppInfo.mm
@@ -0,0 +1,34 @@
+#include "AppInfo.h"
+
+static AppInfo* g_sAppInfo = NULL;
+
+AppInfo* AppInfo::sharedAppInfo()
+{
+    if (g_sAppInfo == NULL)
+    {
+        g_sAppInfo = new AppInfo();
+    }
+    return g_sAppInfo;
+}
+
+const std::string& AppInfo::getVersionName()
+{
+    if (m_strVersionName != "0.00.00")
+        return m_strVersionName;
+    
+    // 获取版本号
+    NSString* versionName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    m_strVersionName = versionName.UTF8String;
+    return m_strVersionName;
+}
+
+int AppInfo::getVersionCode()
+{
+    if (m_nVersionCode > 0)
+        return m_nVersionCode;
+    
+    // 获取编译版本号
+    NSString* versionCode = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleVersionKey];
+    m_nVersionCode = [versionCode intValue];
+    return m_nVersionCode;
+}
